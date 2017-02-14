58f3b94c3dc05bdbf888b3cabac84cead4a12755
Lars Liden <laliden@microsoft.com>
2017-02-14 15:01:49 -0800

    Added blisdk.js

---
+++ built/client/Blisdk.js
@@ -0,0 +1,5 @@
+var BlisRecognizer = require("./BlisRecognizer");
+var BlisDialog = require("/BlisDialog");
+exports.BlisRecognizer = BlisRecognizer;
+exports.BlisDialog = BlisDialog;
+//# sourceMappingURL=Blisdk.js.map
---
+++ src/client/Blisdk.ts
@@ -0,0 +1,4 @@
+var BlisRecognizer = require("./BlisRecognizer");
+var BlisDialog = require("/BlisDialog");
+exports.BlisRecognizer = BlisRecognizer;
+exports.BlisDialog = BlisDialog;
