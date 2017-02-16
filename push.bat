2f3beb807b813c152c919254bf5422638eea4d9c
Lars Liden <laliden@microsoft.com>
2017-02-16 10:33:44 -0800

    test5

--- push.bat
+++ push.bat
@@ -1,1 +1,1 @@
-git add * && git commit -m "%*" && git push && C:\codebox\Blis\BotTest> npm install --save github:Microsoft/BLIS-SDK
+git add * && git commit -m "%*" && git push && START cmd.exe /k "cd C:\codebox\Blis\BotTest & npm install --save github:Microsoft/BLIS-SDK && exit"
