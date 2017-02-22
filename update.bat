8bd81c73fa7ae55ef75b82a948d3ba4159623702
Lars Liden <laliden@microsoft.com>
2017-02-22 09:09:20 -0800

    updated build commands

--- update.bat
+++ update.bat
@@ -1,1 +1,1 @@
-START cmd.exe /k "cd C:\codebox\Blis\BotTest & npm install --save github:Microsoft/BLIS-SDK && exit"
+START cmd.exe /k "cd C:\codebox\Blis\BLIS-SDK-SAMPLE & npm install github:Microsoft/BLIS-SDK && exit"
