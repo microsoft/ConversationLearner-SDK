3b0f98bea0f78fb79b165c110919e92f4d7d0016
Lars Liden <laliden@microsoft.com>
2017-02-22 09:21:07 -0800

    readd build files

---
+++ built/Blisdk.js
@@ -0,0 +1,6 @@
+"use strict";
+var BlisRecognizer_1 = require("./BlisRecognizer");
+var BlisDialog_1 = require("./BlisDialog");
+exports.BlisRecognizer = BlisRecognizer_1.BlisRecognizer;
+exports.BlisDialog = BlisDialog_1.BlisDialog;
+//# sourceMappingURL=Blisdk.js.map
