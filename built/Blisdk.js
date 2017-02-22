f944299212ea590ef204236c2d44df56e8147ec8
Lars Liden <laliden@microsoft.com>
2017-02-22 09:25:48 -0800

    rename built to lib

--- built/Blisdk.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var BlisRecognizer_1 = require("./BlisRecognizer");
-var BlisDialog_1 = require("./BlisDialog");
-exports.BlisRecognizer = BlisRecognizer_1.BlisRecognizer;
-exports.BlisDialog = BlisDialog_1.BlisDialog;
-//# sourceMappingURL=Blisdk.js.map
