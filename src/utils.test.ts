02ddbb61af1df1aa420fd8fda2d81fd6fcf12b26
Matt Mazzola <mattm@microsoft.com>
2018-01-25 15:05:35 -0800

    Enable Travis CI with build and test using Jest

---
+++ src/utils.test.ts
@@ -0,0 +1,16 @@
+import { Utils as utilities } from './Utils'
+
+describe('utilities', () => {
+    describe('PrebuiltDisplayText', () => {
+        test('given prebuilt type starts with encyclopediea should return entityText', () => {
+            // Arrange
+            const expected = 'randomValue'
+
+            // Act
+            const actual = utilities.PrebuiltDisplayText('builtin.encyclopedia', null, expected)
+
+            // Assert
+            expect(actual).toEqual(expected)
+        })
+    })
+})
