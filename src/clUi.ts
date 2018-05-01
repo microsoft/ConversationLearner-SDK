9fb6a5c1971f001a42892de4dbbc00d594f7d358
Matt Mazzola <mattm@microsoft.com>
2018-05-01 15:20:25 -0700

    Update import statements

--- src/clUi.ts
+++ src/clUi.ts
@@ -4,11 +4,11 @@
  */
 import * as express from 'express'
 import * as http from 'http'
-import * as clUI from 'conversationlearner-ui'
+import * as clUI from '@conversationlearner/ui'
 
 export default function(port: number = 5050): { app: express.Express; listener: http.Server } {
-    console.log(`conversationlearner-ui directory path: `, clUI.directoryPath)
-    console.log(`conversationlearner-ui default file path: `, clUI.defaultFilePath)
+    console.log(`@conversationlearner/ui directory path: `, clUI.directoryPath)
+    console.log(`@conversationlearner/ui default file path: `, clUI.defaultFilePath)
 
     const app = express()
     app.use(express.static(clUI.directoryPath)).use((req, res) => res.sendFile(clUI.defaultFilePath))
