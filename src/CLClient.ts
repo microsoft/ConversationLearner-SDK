c14a7259a5b0c4b6352a50a7c2d2d34f3999eb3e
Matt Mazzola <mattm@microsoft.com>
2018-07-03 14:57:07 -0700

    fix: remove deprecations and use desctructuring for params

--- src/CLClient.ts
+++ src/CLClient.ts
@@ -254,10 +254,9 @@ export class CLClient {
      * Create a new application
      */
     // TODO: Fix API to return full object
-    public async AddApp(app: models.AppBase, query: string): Promise<string> {
+    public async AddApp(app: models.AppBase, query: string): Promise<models.AppBase> {
         const apiPath = `app`
-        const appResponse = await this.send<models.AppBase>('POST', this.MakeURL(apiPath, query), app)
-        return appResponse.appId
+        return await this.send<models.AppBase>('POST', this.MakeURL(apiPath, query), app)
     }
 
     /** 
@@ -270,7 +269,7 @@ export class CLClient {
     }
 
     /** Retrieves details for a specific $appId */
-    public GetAppStatus(appId: string): Promise<models.AppBase> {
+    public GetArchivedApp(appId: string): Promise<models.AppBase> {
         let apiPath = `archive/${appId}`
         return this.send('GET', this.MakeURL(apiPath))
     }
