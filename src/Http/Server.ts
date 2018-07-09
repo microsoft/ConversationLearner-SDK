1c9e2c8f18790f3b59548c2cefb8a0f8551e0f23
Matt Mazzola <mattm@microsoft.com>
2018-07-09 15:01:56 -0700

    fix: udpate ui and restore memkey check (#300)
    * fix: restore memory key error
    
    * chore: udpate ui

--- src/Http/Server.ts
+++ src/Http/Server.ts
@@ -119,15 +119,7 @@ export const getRouter = (client: CLClient, options: ICLClientOptions): express.
     /** Sets the current active application */
     router.put('/state/app', async (req, res, next) => {
         try {
-            // TODO: Fix the UI page refresh to prevent calling this endpoint without a memory key.
-            /**
-             * During page refresh when UI is still on app page it will call /state/app when the component renders
-             * t then detects no app and redirects back to homepage to call /botinfo and get a userid / memory key 
-             * However, it is too late. The call has already been made and it will return error panel since this request
-             * fails without memory key header
-             */
-            // const key = getMemoryKey(req)
-            const key = req.header(models.MEMORY_KEY_HEADER_NAME)!
+            const key = getMemoryKey(req)
             const app: models.AppBase = req.body
 
             const memory = CLMemory.GetMemory(key)
