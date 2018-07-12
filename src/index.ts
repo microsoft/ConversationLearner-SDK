2503f5a83a5575e5f1ea7a220c71a98068f81218
Matt Mazzola <mattm@microsoft.com>
2018-07-12 13:24:00 -0700

    fix: export callback types and update comments

--- src/index.ts
+++ src/index.ts
@@ -9,5 +9,16 @@ import { RedisStorage } from './RedisStorage'
 import { FileStorage } from './FileStorage'
 import startUiServer from './clUi'
 import { SessionEndState } from '@conversationlearner/models'
+import { OnSessionStartCallback, OnSessionEndCallback } from './CLRunner'
 
-export { startUiServer, ConversationLearner, ICLOptions, ClientMemoryManager, RedisStorage, FileStorage, SessionEndState }
+export {
+    startUiServer,
+    ConversationLearner,
+    ICLOptions,
+    ClientMemoryManager,
+    RedisStorage,
+    FileStorage,
+    SessionEndState,
+    OnSessionStartCallback,
+    OnSessionEndCallback
+}
