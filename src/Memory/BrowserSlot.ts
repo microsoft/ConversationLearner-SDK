7159e94f61243b62a9bdcc9df995661e621cc057
Matt Mazzola <mattm@microsoft.com>
2019-08-07 09:02:59 -0700

    chore: add comment to browser slot

--- src/Memory/BrowserSlot.ts
+++ src/Memory/BrowserSlot.ts
@@ -33,7 +33,9 @@ export class BrowserSlotState {
         return {
             browserId: browserId,
             lastUsed: new Date().getTime(),
-            id: String.fromCharCode(offset + 65),
+            // 0 1 2 3 4 5 6 7 8 9 10
+            // A B C D E F G H I J K
+            id: String.fromCharCode(offset + 'A'.charCodeAt(0)),
         }
     }
 
